7c807fcc03f9c485b3c4a6b325427cb589e679e2
sonal <sonalkarle01@gmail.com>
2021-03-20 09:30:05 +0530

    Welcome'

---
+++ Day26Emppayroll/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayroll
+{
+    public class Employee
+    {
+        public int  ID { get; set; }
+        public string Name { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public string Gender { get; set; }
+        public double Phonenumber { get; set; }
+
+
+        public string Address { get; set; }
+       
+        public string Department { get; set; }
+        public double BasicPay { get; set; }
+        public int Deduction { get; set; }
+        public int TaxablePay { get; set; }
+        public int IncomeTax { get; set; }
+        public int NetPay { get; set; }
+        public int Salary { get; set; }
+    }
+}
